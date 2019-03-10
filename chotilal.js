f98c50e5cb0fb64b962b980095482bf3dc65a085
sagar <mahisagar44@gmail.com>
2019-03-10 07:45:26 +0530

    kodi

---
+++ chotilal.js
@@ -0,0 +1,56 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var app = require('express')();
+var bodyParser = require('body-Parser');
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {
+        case '/chotilal2.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200,  {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        case '/chotilal3.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });
+            break;  
+            case '/chotilal2.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });
+            break;
+    }  
+});  
+server.listen(8080); 
